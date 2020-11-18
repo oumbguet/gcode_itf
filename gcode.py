4cb037911108d1b63745f98ca744eb147fbaf6a5
Florian <florian@localhost.localdomain>
2020-11-18 11:58:12 +0100

    [~] Adding interface functions

--- gcode.py
+++ gcode.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 #G90 Positionnement absolu
 #G91 Positionnement relatif
 #G00 FAST
@@ -61,6 +63,16 @@ class Gcode():
         )
         print(gcode)
 
+    # Move absolute
+    def move_to(self, x, y, z):
+        gcode=(
+            """G90\nG01 X""" + str(x) +
+            """ Y""" + str(y) +
+            """ Z""" + str(z) +
+            """\nG90\n"""
+        )
+        print(gcode)
+
     # Speed
     def set_speed(self, val):                   # Set moving speedrate F
         gcode=(
@@ -79,5 +91,35 @@ class Gcode():
         for line in command.splitlines():       # For each line of the gcode command
             self.serialPort.write(line);        # Send line to the serial port
 
+
+def move_to(gcode, args):
+    if (len(args) == 5):
+        gcode.move_to(int(args[2]), int(args[3]), int(args[4]))
+
+def move_relative(gcode, args):
+    if (len(args) == 5):
+        gcode.move_X(int(args[2]));
+        gcode.move_Y(int(args[3]));
+        gcode.move_Z(int(args[4]));
+
+def display_usage():
+    print("\nUSAGE\n\n\t./gcode.py (move_to|move|move_X|move_Y|move_Z) [arg1] [arg2]...\n\n");
+    print("\tmove_to\t\tX_axis Y_axis Z_axis");
+    print("\t\tMove to absolute coordinates.")
+    print("\n\tmove\t\tX_axis Y_axis Z_axis");
+    print("\t\tMove to relative coordinates.")
+    print("\n\tmove_(X|Y|Z)\trelative_offset_value");
+    print("\t\tMove only one axis by relative offset.\n")
+
 if __name__=="__main__":
     gcode = Gcode()
+    args = sys.argv;
+    functions = {
+        'move_to':move_to,
+        'move':move_relative
+    };
+
+    if (len(args) > 1):
+        functions[args[1]](gcode, args);
+    else:
+        display_usage();
